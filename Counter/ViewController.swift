af19cc30b411fc85dd765d21427dad672e409212
atravka <atravka@gmail.com>
2025-01-09 22:35:32 +0300

    base version with one button

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var clickButton: UIButton!
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    var counter: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counterLabel.text = "\(counter)"
+        
+        clickButton.backgroundColor = UIColor.lightGray
+        clickButton.tintColor = UIColor.white
     }
 
-
+    @IBAction func buttonDidTap(_ sender: Any) {
+        counter += 1
+        counterLabel.text = "Значение счётчика: \n \(counter)"
+    }
+    
 }
 
