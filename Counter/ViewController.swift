d56ea01fdbd82be183a0a60540c2514cb0ea61fc
atravka <atravka@gmail.com>
2025-01-10 00:09:15 +0300

    extended version with 3 button & history

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,7 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var clickButton: UIButton!
+    @IBOutlet weak var clickPlusButton: UIButton!
+    
+    @IBOutlet weak var clickMinusButton: UIButton!
+    
+    @IBOutlet weak var clickResetButton: UIButton!
+    
+    @IBOutlet weak var historyUITextView: UITextView!
     
     @IBOutlet weak var counterLabel: UILabel!
     
@@ -20,14 +26,37 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         counterLabel.text = "\(counter)"
         
-        clickButton.backgroundColor = UIColor.lightGray
-        clickButton.tintColor = UIColor.white
+        historyUITextView.text = "История изменений:"
     }
 
-    @IBAction func buttonDidTap(_ sender: Any) {
+    @IBAction func buttonPlusDidTap(_ sender: Any) {
         counter += 1
         counterLabel.text = "Значение счётчика: \n \(counter)"
+        historyUITextView.text = historyUITextView.text + "\n [\(Date().printMyFormat())]: значение изменено на +1"
+    }
+    
+    @IBAction func buttonMinusDidTap(_ sender: Any) {
+        counter -= 1
+        if counter < 0 {
+            counter = 0
+            historyUITextView.text = historyUITextView.text + "\n [\(Date().printMyFormat())]: попытка уменьшить значение счётчика ниже 0"
+        }
+        counterLabel.text = "Значение счётчика: \n \(counter)"
+        historyUITextView.text = historyUITextView.text + "\n [\(Date().printMyFormat())]: значение изменено на -1"
+    }
+    
+    @IBAction func buttonResetDidTap(_ sender: Any) {
+        counter = 0
+        counterLabel.text = "Значение счётчика: \n \(counter)"
+        historyUITextView.text = historyUITextView.text + "\n [\(Date().printMyFormat())]: значение сброшено"
     }
     
 }
 
+extension Date {
+    func printMyFormat() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYY-MM-DD HH:mm:ss"
+        return dateFormatter.string(from: self)
+    }
+}
